c29959bae81741743a80df0fe745eb1ad2afd546
anurag-verma <anuragverma394@gmail.com>
2024-02-02 14:52:44 +0530

    first commit

---
+++ CommandLine1.java
@@ -0,0 +1,9 @@
+class CommandLine1 {
+    public static void main(String arg[])
+    {
+        System.out.println("Name1\t"+arg[0]);
+        System.out.println("Name2\t"+arg[1]);
+        System.out.println("Name3\t"+arg[2]);
+
+    }
+}
